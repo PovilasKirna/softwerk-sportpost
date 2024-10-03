d20075233e2b129cfe9a31d06ce01a228ff7ca6e
Povilas Kirna <povilas.kirna@gmail.com>
2024-10-03 14:12:51 +0200

    feat: add ui items, streamline providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import HeaderAuth from '@/components/header-auth';
-import { ThemeSwitcher } from '@/components/theme-switcher';
 import { GeistSans } from 'geist/font/sans';
-import { ThemeProvider } from 'next-themes';
 import './globals.css';
-import { Toaster } from 'sonner';
 import { AuthToast } from './auth-toast';
+import Providers from '@/components/providers';
 
 const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
 
@@ -18,20 +16,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en" className={GeistSans.className} suppressHydrationWarning>
             <body className="bg-background text-foreground">
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                <Providers>
                     <main className="flex min-h-screen flex-col items-center">
                         <div className="flex w-full flex-1 flex-col items-center gap-20">
                             <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
                                 <div className="flex w-full max-w-5xl items-center justify-between p-3 px-5 text-sm">
                                     <HeaderAuth />
-                                    <Toaster richColors closeButton expand />
                                     <AuthToast />
                                 </div>
                             </nav>
                             <div className="flex max-w-5xl flex-col gap-20 p-5">{children}</div>
                         </div>
                     </main>
-                </ThemeProvider>
+                </Providers>
             </body>
         </html>
     );
---
+++ components/providers.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Toaster } from './ui/sonner';
+import { ThemeProvider } from 'next-themes';
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        // With SSR, we usually want to set some default staleTime
+                        // above 0 to avoid refetching immediately on the client
+                        staleTime: 60 * 1000,
+                        refetchOnWindowFocus: false,
+                        refetchOnMount: false,
+                    },
+                },
+            }),
+    );
+
+    return (
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <QueryClientProvider client={queryClient}>
+                <Toaster richColors closeButton expand />
+                {children}
+            </QueryClientProvider>
+        </ThemeProvider>
+    );
+}
